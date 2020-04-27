fd386f21fb7600f7563a0457f684de6f06514909
himansh-nagar <himanshu19@navgurukul.org>
2020-04-27 10:57:41 +0530

    upload

---
+++ calculator.py
@@ -0,0 +1,33 @@
+def operate(num1, operator, num2):
+    if operator=='+':
+        return num1 + num2
+    elif operator=='-':
+        return num1 - num2
+    elif operator=='*':
+        return num1 * num2
+    else:
+        return num1 / num2
+
+def solve(question_list):
+    if len(question_list)==1:
+        return int(question_list[0])
+    elif len(question_list)==3:
+        return operate(int(question_list[0]), question_list[1], int(question_list[2]))
+    else:
+        return operate(solve(question_list[:-2]), question_list[-2], int(question_list[-1]))
+
+
+user=input()
+question=[]
+operators=['-','+','*','/']
+s=''
+for i in user:
+    if i not in operators:
+        s+=i
+    elif i in operators:
+        question.append(s)
+        question.append(i)
+        s=''
+question.append(s)
+
+print(solve(question))
